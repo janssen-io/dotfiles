a84b94ff7eb7edf5e1fc45dd95b2a2a8916d0476
Stan Janssen <stanjanssen@outlook.com>
2014-11-17 23:08:21 +0100

    cp files

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-for f in * .[^.]*; do
-    ln -s $f ~/$f
+HOME_FILES=".bashrc .i3 .vimrc xrandr.sh .Xresources"
+
+if [ ! -e ~/.dotbackup ]; then
+    mkdir ~/.dotbackup
+fi
+
+for f in $HOME_FILES; do
+    if [ -e ~/$f ] && [ ! -h ~/f ]; then
+        mv ~/$f ~/.dotbackup/
+    fi
+    # ln -s $f ~/$f
+    cp $f ~/$f
 done
 
 echo "Done!" 
